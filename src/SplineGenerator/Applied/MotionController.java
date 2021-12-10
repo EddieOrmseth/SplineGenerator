bf0fd285883130af9f20c35ff71261fe1f9e30e4
Eddie <eddie.ormseth@icloud.com>
2021-12-10 09:12:55 -0700

    JavaDoc MotionController

--- src/SplineGenerator/Applied/MotionController.java
+++ src/SplineGenerator/Applied/MotionController.java
@@ -5,41 +5,86 @@ import SplineGenerator.Util.DVector;
 
 import java.util.function.Supplier;
 
+/**
+ * A controller for complete motion, includes direction and velocity;
+ */
 public class MotionController {
 
+    /**
+     * The direction controller
+     */
     private Navigator.Controller navigationController;
+
+    /**
+     * The velocity controller
+     */
     private VelocityController velocityController;
 
+    /**
+     * Optional: the object that supplies the current position;
+     */
     private Supplier<DPoint> positionSupplier;
 
+    /**
+     * A constructor that requires a position supplier
+     *
+     * @param navigationController The direction controller to be followed
+     * @param velocityController The velocity controller to be followed
+     * @param positionSupplier The position supplier
+     */
     public MotionController(Navigator.Controller navigationController, VelocityController velocityController, Supplier<DPoint> positionSupplier) {
         this.navigationController = navigationController;
         this.velocityController = velocityController;
         this.positionSupplier = positionSupplier;
     }
 
+    /**
+     * A constructor that does not require a position supplier
+     *
+     * @param navigationController The direction controller to be followed
+     * @param velocityController The velocity controller to be followed
+     */
     public MotionController(Navigator.Controller navigationController, VelocityController velocityController) {
         this.navigationController = navigationController;
         this.velocityController = velocityController;
     }
 
+    /**
+     * A method for updating the controllers, the position is retrieved from the position supplier
+     */
     public void update() {
         navigationController.update(positionSupplier.get());
         velocityController.update();
     }
 
+    /**
+     * A method for updating the controllers
+     *
+     * @param position The position that is fed to the navigation controller
+     */
     public void update(DPoint position) {
         navigationController.update(position);
         velocityController.update();
     }
 
+    /**
+     * A method for getting the motion output of the controllers
+     *
+     * @return The desired motion
+     */
     public DVector getMotion() {
         DVector motion = navigationController.getDirection();
         motion.setMagnitude(velocityController.getVelocity());
         return motion;
     }
 
+    /**
+     * A method to determine of the navigator has finished its path
+     *
+     * @return Whether or not the navigator has finished its path
+     */
     public boolean isFinished() {
         return navigationController.isFinished();
     }
+
 }
